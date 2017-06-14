dedf8621f3e99fa7720295ba98a0c3d720c977fc
建溢 梁 <a.124p@live.com>
2017-06-14 23:58:55 +0800

    变更集1

---
+++ DigitalNotation/BinaryToDecimalParsec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScienceComputation {
+	public static class DigitalNotation {
+		/// <summary>
+		/// 二进制转换十进制
+		/// </summary>
+		/// <param name="binary"></param>
+		/// <returns></returns>
+		public static string BinaryToDecimalParsec( string binary ) {
+			double result = 0.0;
+			for (int index = 0, pow = binary.Length - 1 ; index < binary.Length ; index++) {
+				result += Math.Pow( binary[ index ] , pow );
+			}
+			return result.ToString( );
+		}
+	}
+}
